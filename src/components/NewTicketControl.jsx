0f6c90ca4c9b06effd8a09cbb1dee855f1b6a7c3
Panatda Inthala <panatdainthala@gmail.com>
2018-04-24 16:22:57 -0700

    it will have add state management.

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
+import ConfirmationQuestions from './ConfirmationQuestions';
+import NewTicketForm from './NewTicketForm';
 
 class NewTicketControl extends React.Component {
-  constructor(props) {
-  super(props);
-  this.state = {
-    formVisibleOnPage: false
-  };
-  this.handleClick = this.handleClick.bind(this);
-}
 
-  handleClick() {
-    this.setState({ formVisibleOnPage: true });
-    console.log(
-      "formVisibleOnPage is currently set to:" + this.state.formVisibleOnPage
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+    this.handleTroubleshootingConfirmation = this.handleTroubleshootingConfirmation.bind(this);
   }
 
-  render() {
+  handleTroubleshootingConfirmation(){
+    this.setState({formVisibleOnPage: true});
+  }
+
+  render(){
+    let currentlyVisibleContent = null;
+    if (this.state.formVisibleOnPage){
+      currentlyVisibleContent = <NewTicketForm />;
+    } else {
+      currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>;
+    }
     return (
       <div>
-        <p>This is the NewTicketControl component!</p>
-        <strong onClick={this.handleClick}>Click me to change my state!</strong>
+        {currentlyVisibleContent}
       </div>
     );
   }
